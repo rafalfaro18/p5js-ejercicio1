56d5caf4a8e9431743ae6148e091adab904aafb0
Rafael Alfaro <rafalfaro_18@outlook.com>
2017-05-27 19:23:05 -0600

    Setup de ejercicio

--- sketch.js
+++ sketch.js
@@ -1,7 +1,13 @@
-function setup() {
+var col=0;
 
+function setup() {
+	createCanvas(600, 400);
 }
 
 function draw() {
-  
+  //background
+  background(col);
+  //ellipse
+  fill(250, 118, 222);
+  ellipse(mouseX, 200, 64, 64);
 }
